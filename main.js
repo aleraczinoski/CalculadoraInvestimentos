e95f07e00ef9459051f5b39a3ab01e46d4454231
Alexandre Raczinoski <alexandreraczinoski@gmail.com>
2026-04-23 19:37:07 -0300

    Validação do formulário finalizada

--- main.js
+++ main.js
@@ -4,18 +4,25 @@ const form = document.getElementById("investment-form");
 
 function renderProgression(event) {
   event.preventDefault();
+  if (document.querySelector(".error")) {
+    return;
+  }
 
   const startingAmount = Number(
-    document.getElementById("starting-amount").value,
+    document.getElementById("starting-amount").value.replace(",", "."),
   );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value,
+    document.getElementById("additional-contribution").value.replace(",", "."),
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", "."),
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", "."),
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -27,6 +34,40 @@ function renderProgression(event) {
   );
 
   console.log(returnsArray);
+}
+
+function validateInput(event) {
+  if (event.target.value === "") {
+    return;
+  }
+  const { parentElement } = event.target;
+  const grandParentElement = event.target.parentElement.parentElement;
+  const inputValue = event.target.value.replace(",", ".");
+
+  if (
+    (isNaN(inputValue) || Number(inputValue) <= 0) &&
+    !parentElement.classList.contains("error")
+  ) {
+    const errorTextElement = document.createElement("p");
+    errorTextElement.classList.add("text-red-500");
+    errorTextElement.textContent = "Insira um valor numérico e maior que zero";
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains("error") &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove("error");
+    grandParentElement.querySelector("p").remove();
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
 }
 
 form.addEventListener("submit", renderProgression);
